98e36adae7e1b196be3622449f525252da27cd0d
Samuel Xu <me@samuelxu.com>
2019-02-04 11:13:29 -0500

    changed expand-image to generalize for any similar aspect ratios

--- expand_image.cc
+++ expand_image.cc
@@ -1,34 +1,69 @@
 #include <cstdio>
+#include <cmath>
 #include "bitmap_image.hpp"
 using namespace std;
+
+bool isInt(float num) {
+    return floor(num) == ceil(num);
+}
 
 // expand_image(bitmap_image, bitmap_image):
 // recieves the old image and image diff as parameters to 
 // construct the image of higher resolution.
 void expand_image(bitmap_image oldImg, bitmap_image diff) {
-    bitmap_image newImg(1280,720);
-    int newH = 720, newW = 1280;
-    if (true) {
-        for (int y=0; y<newH; ++y) {
-            if (y%2 == 0) {
-                for (int x=0; x<newW; ++x) {
-                    if (x%2 == 0) {
-                        newImg.set_pixel(x,y,oldImg.get_pixel(x/2,y*2/3));
-                    }
-                    else {
-                        newImg.set_pixel(x,y,diff.get_pixel(x,y));
-                    }
-                }
-            } else {
-                for (int x=0; x<newW; ++x) {
-                    newImg.set_pixel(x,y,diff.get_pixel(x,y));
-                }
+    int diffW = diff.width();
+    int diffH = diff.height();
+    int cur = diffW;
+    int curSqrt = sqrt(cur);
+
+    while (true) {
+        if (isInt(curSqrt)) {
+            smallSqrt = cur - diffW;
+            if (isInt(smallSqrt)) {
+                break;
             }
         }
+        ++cur;
+        curSqrt = sqrt(cur);
     }
-    else {
-        throw std::logic_error("Not implemented for this res");
-    }
+
+    int loFactor = round(sqrt(cur-diffW));
+    int hiFactor = round(curSqrt);
+
+    int newW = oldImg.width() * hiFactor / loFactor;
+    int newH = oldImg.height() * hiFactor / loFactor;
+
+    bitmap_image newImg(newW, newH);
+
+    int diffX = 0;
+    int diffY = 0;
+
+    for (std::size_t y=0; y<height; y+= hiFactor) {
+      for (std::size_t x=0; x<width; x+= hiFactor) {
+         // iterating through inner block pixels, innerX and innerY indicate the current position of the block we are at.
+         // increment diffY, reset diffX to 0 each time we go to a new block
+         ++diffY;
+         diffX = 0;
+         for (int innerX = x; innerX < x+hiFactor; ++innerX) {
+            for (int innerY = y; innerY < y+hiFactor; ++innerY) {
+               // only get and set pixel if the block is not included in the old block (for now it is the top left smaller square with sides of length "loFactor")
+               if (!(innerX < x+loFactor) || !(innerY < y+loFactor)) {
+                  // grab pixel from the diff
+                  newImg.set_pixel(innerX, innerY, diff.get_pixel(diffX, diffY));
+                  // increment diffX every time we increment through the inner block.
+                  ++diffX;
+               }
+               else {
+                   // grab pixel from the old image
+                   newImg.set_pixel(innerX,innerY, oldImg.get_pixel(x * loFactor/hiFactor + (innerX - x), 
+                                                                    y * loFactor/hiFactor + (innerY - y)));
+               }
+               
+            }
+         }
+      }
+   }
+
     newImg.save_image("expanded.bmp");
 }
 
