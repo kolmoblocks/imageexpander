e7bacb17bb0dba27f3cf0340c14649b26f20fbe2
William Tang <williamjt1998@gmail.com>
2019-03-06 10:23:45 -0500

    Updates

--- diffDeltas.cpp
+++ diffDeltas.cpp
@@ -116,12 +116,18 @@ void generateDiff (const char *lowRes, const char *highRes){
                   // set pixel of the diff at diffX , diffY with the color at the highResImage at innerX , innerY
                   diffVec.push_back(highResImage.at((innerX+innerY*highResWidth)*3));
                 fwrite(&highResImage.at((innerX+innerY*highResWidth)*3), 1, 1, f);
+                unsigned char r,g,b;
+                r = (unsigned int)highResImage.at((innerX+innerY*highResWidth)*3);
+                // g = highResImage.at((innerX+innerY*highResWidth)*3+1);
+                // b = highResImage.at((innerX+innerY*highResWidth)*3+2);
+                cout<<r<<endl;
+                 
 
 
 
 
-                  diffVec.push_back(highResImage.at((innerX+innerY*highResWidth)*3+1));
-                  diffVec.push_back(highResImage.at((innerX+innerY*highResWidth)*3+2));
+                //   diffVec.push_back(highResImage.at((innerX+innerY*highResWidth)*3+1));
+                //   diffVec.push_back(highResImage.at((innerX+innerY*highResWidth)*3+2));
                 
                 
 
--- writingBytes.cpp
+++ writingBytes.cpp
@@ -1,46 +1,76 @@
 #include <fstream>
 int current_bit = 0;
 unsigned char bit_buffer;
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <cmath>
+
 
 FILE *f;
 
 void WriteBit (int bit) {
  f = fopen ("random.dat", "w");
-  if (bit)
-    bit_buffer |= (1<<current_bit);
+	if (bit)
+		bit_buffer |= (1<<current_bit);
 
-  current_bit++;
-  if (current_bit == 8)
-  {
-    fwrite (&bit_buffer, 1, 1, f);
-    current_bit = 0;
-    bit_buffer = 0;
-  }
+	current_bit++;
+	if (current_bit == 8)
+	{
+		fwrite (&bit_buffer, 1, 1, f);
+		current_bit = 0;
+		bit_buffer = 0;
+	}
 }
 
 
 
 void Flush_Bits(){
-    while (current_bit < 8) {
-        bit_buffer = (bit_buffer<<1);
-          current_bit++;
-    }
-    fwrite (&bit_buffer, 1, 1, f);
-    fclose (f); 
+		while (current_bit < 8) {
+				bit_buffer = (bit_buffer<<1);
+					current_bit++;
+		}
+		fwrite (&bit_buffer, 1, 1, f);
+		fclose (f); 
+
+}
+
+int bits[8];
+		vector<int> v;
+
+
+void Trim_Leading(int bits) {
+		unsigned char c = 2;		
+
+		int i =8;
+		while(i >= 8 - bits){
+				if ((c & (1 << i)) != 0) {
+					v.push_back(1);
+					i--;
+					break;
+				}
+				i--;
+		}
+		for ( ; i >= 0 ; i--) {
+						cout<<i;
+
+				v.push_back((c & (1 << i)) != 0);
+		}
+
+
 
 }
 
 int main (){
-    WriteBit(0);
-    WriteBit(0);
-    WriteBit(0);
-    WriteBit(0);
-    WriteBit(1);
-
-
-    //need to fill the 0bits from the right and not left
-    
-    Flush_Bits();
+		Trim_Leading(6);
+		cout<<endl;
+		for (auto it : v){
+				cout<<it;
+		}
+		cout<<endl;
+		//need to fisll the 0bits from the right and not left
+		
+		//Flush_Bits();
 }
-   
-   //xxd -b random.dat
+	 
+	 //xxd -b random.dat
