ecfdfd22c63aebbf2f94797230274074ed21a193
Samuel Xu <me@samuelxu.com>
2019-03-08 10:40:22 -0500

    added diffMap file

---
+++ diffMap.cpp
@@ -0,0 +1,17 @@
+#include "lodepng.h"
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <unordered_set> 
+#include <numeric> 
+#include <string>
+
+void writeMBlock(char *buf, std::vector<unsigned char>::iterator start, std::vector<unsigned char>::iterator end) {
+    std::vector<unsigned char>::pos = start;
+    char id = 'M';
+    fwrite()
+    while (pos != end) {
+        
+        ++pos;
+    }
+}
