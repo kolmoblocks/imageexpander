8532dfe1b5572ac16858c0c517d966c7cbce9565
Samuel Xu <me@samuelxu.com>
2019-03-21 16:44:02 -0400

    huffman fixes

--- huffman/huffman.cpp
+++ huffman/huffman.cpp
@@ -3,46 +3,49 @@
 //
 
 #include "huffman.h"
+#define TOKENSIZE 1024
 
 
 struct compare{
     bool operator()(dNode *left, dNode *right) {
-        return left->weight < right->weight;
+        return left->weight > right->weight;
     }
 };
 
 
-void inOrder(dNode *curNode, std::vector<unsigned char> bitstring, std::vector<unsigned char> (&encTable)[256]) {
+void inOrder(dNode *curNode, std::vector<unsigned char> bitstring, std::vector<unsigned char> (&encTable)[TOKENSIZE]) {
     if (curNode == nullptr) {
-        std::cout << "0" << std::endl;
         return;
     } else if (!(curNode->right) && !(curNode->left)) {
         // if node is a leaf, add the bitstring to the encTable
-        std::cout << "0.9" << std::endl;
-        std::cout << curNode->weight << " " << (int)(curNode->ch) << std::endl;
+        //std::cout << "0.9" << std::endl;
+        //std::cout << curNode->weight << " " << (int)(curNode->ch) << std::endl;
         int c = curNode->ch;
-        std::cout << "1";
+        //std::cout << "1";
         encTable[c].insert(encTable[c].end(), bitstring.begin(), bitstring.end());
-        std::cout << "2" << std::endl;
+        if (bitstring.size() < 7) std ::cout << bitstring.size() << std::endl;
+
+        //std::cout << "2" << std::endl;
         return;
 
     } else {
-        std::cout << "3" << std::endl;
+        //std::cout << "3" << std::endl;
         // if node is a char, add the bitstring
         bitstring.push_back(0);
         inOrder(curNode->left, bitstring, encTable);
         bitstring.pop_back();
-        std::cout << "4" << std::endl;
+        //std::cout << "4" << std::endl;
         bitstring.push_back(1);
         inOrder(curNode->right, bitstring, encTable);
         bitstring.pop_back();
-        std::cout << "5" << std::endl;
+        //std::cout << "5" << std::endl;
         return;
     }
 }
 
 dNode *encodeHuffman(std::vector<unsigned char> &rawData) {
-    int charFreq[256] = {0};
+    std::cout << "huff" << std::endl;
+    int charFreq[TOKENSIZE] = {0};
     int rawDataSize = rawData.size();
 
     if (rawDataSize % 8 != 0) std::cout << "sum ting wong" << std::endl;
@@ -54,7 +57,7 @@ dNode *encodeHuffman(std::vector<unsigned char> &rawData) {
             binVector.push_back(rawData[j]);
         }
         int index = binToInt(binVector);
-        if (index <= 255) {
+        if (index <= TOKENSIZE-1) {
             ++(charFreq[index]);
         }
         binVector.clear();
@@ -62,7 +65,7 @@ dNode *encodeHuffman(std::vector<unsigned char> &rawData) {
 
     std::priority_queue<dNode*, std::vector<dNode*>, compare> mpq;
 
-    for (int i=0; i<256; ++i) {
+    for (int i=0; i<TOKENSIZE; ++i) {
         if (charFreq[i] > 0) {
             mpq.push(new dNode{i, charFreq[i], nullptr, nullptr});
         }
@@ -87,7 +90,7 @@ dNode *encodeHuffman(std::vector<unsigned char> &rawData) {
 
 
     dNode *encodeDictPtr = mpq.top();
-    std::vector<unsigned char> encTable[256];
+    std::vector<unsigned char> encTable[TOKENSIZE];
 
     std::vector<unsigned char> bitstring;
     inOrder(encodeDictPtr, bitstring, encTable);
@@ -97,25 +100,25 @@ dNode *encodeHuffman(std::vector<unsigned char> &rawData) {
     auto leadingIt = rawData.begin();
     auto trailingIt = rawData.begin();
     int i=0;
+
     while ( i<rawDataSize ) {
-        int lim = i+8;
+        int lim = i+10;
         for (; i<lim; ++i) {
             curCharVec.push_back(*leadingIt);
             ++leadingIt;
         }
-        std::cout << curCharVec.size() << std::endl;
         int pos = binToInt(curCharVec);
 
+        //if (encTable[pos].size() > 8){std::cout << encTable[pos].size()<<std::endl ;}
         for (auto it=encTable[pos].begin(); it<encTable[pos].end(); ++it) {
             *trailingIt = *it;
             ++trailingIt;
         }
         curCharVec.clear();
     }
-    std::cout << "before: " << rawDataSize << std::endl;
+    std::cout << "before: " << rawData.size() << std::endl;
     rawData.erase(trailingIt, rawData.end());
     std::cout << "after: " << rawData.size() << std::endl;
-
     return encodeDictPtr;
 }
 
@@ -124,5 +127,5 @@ dNode *encodeHuffman(std::vector<unsigned char> &rawData) {
 
 void decodeHuffman(std::vector<unsigned char> &encoded, std::vector<unsigned char> &decoded, dNode *encodeDict) {
     auto encodedIt = encoded.begin();
-    while (encodedic)
+
 }
