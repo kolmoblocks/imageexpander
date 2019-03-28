0df3bceac97ced44f0976b361e7cac01c5284da6
William Tang <williamjt1998@gmail.com>
2019-03-28 14:47:04 -0400

    Debug

--- enhance/enhance.cpp
+++ enhance/enhance.cpp
@@ -61,7 +61,7 @@ void getPixels(vector<unsigned int> &pixels, vector<unsigned char> &diff, vector
                             refG = lowRes[ 3*((deltaXCpy - 1) * lowFactor/highFactor 
                             + lowResImgW * (deltaYCpy-1) * lowFactor/highFactor ) + 1];
 
-                            refB = lowRes[ 3*((deltaXCpy) * lowFactor/highFactor 
+                            refB = lowRes[ 3*((deltaXCpy-1) * lowFactor/highFactor
                             + lowResImgW * (deltaYCpy-1) * lowFactor/highFactor) + 2];
 
                             // refG = lowRes[3*((1920 * (deltaYCpy-1)-1) + (deltaXCpy-1)) + 1];
@@ -80,6 +80,7 @@ void getPixels(vector<unsigned int> &pixels, vector<unsigned char> &diff, vector
 
                             refR = lowRes[ 3*((deltaXCpy) * lowFactor/highFactor 
                             + lowResImgW * (deltaYCpy-1) * lowFactor/highFactor)];
+
 
                             refG = lowRes[ 3*((deltaXCpy) * lowFactor/highFactor 
                             + lowResImgW * (deltaYCpy-1) * lowFactor/highFactor ) + 1];
@@ -110,26 +111,29 @@ void getPixels(vector<unsigned int> &pixels, vector<unsigned char> &diff, vector
                             deltaXCpy +=1;
                         }
 
+                        if (refR > 255 || refG > 255 || refB > 255 || refR < 0 || refG < 0 || refB < 0)
+                        cout<<refR<<":"<<refG<<":"<<refB<<endl;
 
-
-
-                        r = refR + offset + binToSignedInt(getBits(diff, diffPos, rangeSize));
+                        if (offset != 0) {
+                            cout << "here" << std::endl;
+                        }
+                        r = refR + offset - binToSignedInt(getBits(diff, diffPos, rangeSize));
                         diffPos += rangeSize;
-                        g = refG + offset + binToSignedInt(getBits(diff, diffPos, rangeSize));
+                        g = refG + offset - binToSignedInt(getBits(diff, diffPos, rangeSize));
                         diffPos += rangeSize;
-                        b = refB + offset + binToSignedInt(getBits(diff, diffPos, rangeSize));
+                        b = refB + offset - binToSignedInt(getBits(diff, diffPos, rangeSize));
 
                         diffPos += rangeSize;
 
 
-                        if (r>255){
+                        if (r>255 || r< 0){
                             std::cout << "r: " << r << std::endl;
                         }
-                        if (g>255) {
+                        if (g>255|| g< 0) {
                             std::cout << "g: " << g << std::endl;
                             g = 255;
                         }
-                        if (b>255) {
+                        if (b>255|| b< 0) {
                             std::cout << "b: " << b << std::endl;
                         }
                         pixels.push_back(r);
@@ -214,12 +218,13 @@ void populateBlocks(std::vector<blockParams> &blocks, int width, int height, int
 }
 
 
-void populateDiffPixelVec(std::vector<unsigned char> &diffPixelVec, std::vector<unsigned int> &blocksPixelVec, unsigned deltaUnitSize, unsigned highResWidth, unsigned highResHeight) {
+void populateDiffPixelVec(std::vector<unsigned int> &diffPixelVec, std::vector<unsigned int> &blocksPixelVec, unsigned deltaUnitSize, unsigned highResWidth, unsigned highResHeight) {
     // assuming is RANGE 
     // assuming default config for a 16:9 image
     unsigned blockDataSize = highResHeight*highResWidth / (16*9);
     unsigned numBlocks = 16*9;
 
+    cout<<blocksPixelVec.size()<<endl;
     std::vector<unsigned int> blockPosVec;
     for (int i=0; i<numBlocks; ++i) {
         blockPosVec.push_back(i * blockDataSize * 3);
@@ -275,7 +280,7 @@ bool checkFileHeader(vector<unsigned char>&diff){
     return res == "DIFF";
 }
 
-void expand_image( std::vector<unsigned char> oldImgVec, unsigned oldImgW, unsigned oldImgH, std::vector<unsigned char> &diffVec, unsigned loFactor, unsigned hiFactor, int diffW) {
+void expand_image( std::vector<unsigned char> oldImgVec, unsigned oldImgW, unsigned oldImgH, std::vector<unsigned int> &diffVec, unsigned loFactor, unsigned hiFactor, int diffW) {
 
 
     unsigned newW = oldImgW * hiFactor / loFactor;
@@ -329,8 +334,8 @@ void expand_image( std::vector<unsigned char> oldImgVec, unsigned oldImgW, unsig
 void enhance(char *lowResFileName, char *diffFileName) {
     FILE *pDiff;
     pDiff = fopen(diffFileName, "rb");
-    vector<unsigned char> deltas, diffPixelVec, lowResImage,diff;
-    vector <unsigned int> blocksPixelVec;
+    vector<unsigned char> deltas, lowResImage,diff;
+    vector <unsigned int> blocksPixelVec,diffPixelVec;
     unsigned lowResWidth, lowResHeight, highResWidth, highResHeight,lowFactor, highFactor, 
     unitSize, deltaUnitSize, totalDeltaUnits, error;
     ofstream f2;
